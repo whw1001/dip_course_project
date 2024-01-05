fbe50305a729f8b9195eab406021a86536d34706
whw1001 <whw1001@sjtu.edu.cn>
2024-01-05 18:05:48 +0800

    1-05

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 
 image = Image.new("RGB", (100, 100))
-
+#读取image
 def select_image():
     global image
     file_path = filedialog.askopenfilename()
@@ -13,11 +13,11 @@ def select_image():
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = Image.fromarray(image)
 
-
+# 创建用于GUI展示的image并调整其大小以适应界面
 def showing_image():
     global image
     image_show = image.copy()
-    image_show.thumbnail((300, 300))  # 调整图像大小以适应界面
+    image_show.thumbnail((300, 300))
     photo = ImageTk.PhotoImage(image_show)
     label.config(image=photo)
     label.image = photo
@@ -25,7 +25,7 @@ def showing_image():
 def trigger():
     select_image()
     showing_image()
-    
+
 # 创建主窗口
 root = tk.Tk()
 root.title("Lego元件计数器")
@@ -37,8 +37,6 @@ label.pack(padx=10, pady=10)
 # 创建按钮以选择图片
 select_button = tk.Button(root, text="请选择图片", command=trigger)
 select_button.pack(padx=10, pady=5)
-
-#cv2.imshow('Image', image)
 
 # 运行主循环
 root.mainloop()
