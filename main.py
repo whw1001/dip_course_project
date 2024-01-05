43ef6958e0c3bf512e795469f2547ab52a7d889b
whw1001 <whw1001@sjtu.edu.cn>
2024-01-05 18:02:10 +0800

    写了showing_image()和trigger()函数

--- main.py
+++ main.py
@@ -3,28 +3,43 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
+image = Image.new("RGB", (100, 100))
+
 def select_image():
+    global image
     file_path = filedialog.askopenfilename()
     if file_path:
         image = cv2.imread(file_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = Image.fromarray(image)
-        image.thumbnail((300, 300))  # 调整图像大小以适应界面
-        photo = ImageTk.PhotoImage(image)
-        label.config(image=photo)
-        label.image = photo
 
+
+def showing_image():
+    global image
+    image_show = image.copy()
+    image_show.thumbnail((300, 300))  # 调整图像大小以适应界面
+    photo = ImageTk.PhotoImage(image_show)
+    label.config(image=photo)
+    label.image = photo
+
+def trigger():
+    select_image()
+    showing_image()
+    
 # 创建主窗口
 root = tk.Tk()
-root.title("图片选择器")
+root.title("Lego元件计数器")
 
 # 创建标签以显示图片
 label = tk.Label(root)
 label.pack(padx=10, pady=10)
 
 # 创建按钮以选择图片
-select_button = tk.Button(root, text="选择图片", command=select_image)
+select_button = tk.Button(root, text="请选择图片", command=trigger)
 select_button.pack(padx=10, pady=5)
+
+#cv2.imshow('Image', image)
 
 # 运行主循环
 root.mainloop()
+
