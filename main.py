853208323de9641a2226cbe9f06b4ebe6512c5f9
whw1001 <whw1001@sjtu.edu.cn>
2024-01-15 15:29:19 +0800

    设计思路大改！按照hsv颜色分类

--- main.py
+++ main.py
@@ -9,26 +9,21 @@ image = np.zeros((1000, 1000, 3), dtype=np.uint8)  # Creating a black image with
 # 读取image
 def select_image(path):
     global image
-    #file_path = filedialog.askopenfilename()
     file_path = path
-    #file_path = '30012.png'
     if file_path:
         image = cv2.imread(file_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         h, w, _ = image.shape
-        # cv2.imshow("original_img", cv2.resize(image, (int(w * ratio), int(h * ratio))))
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+
 
 def denoise(img):
     #global denoised_image
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # vimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #img = cv2.equalizeHist(img)
-    denoised_image = cv2.GaussianBlur(img, (9, 9), 0)
+    denoised_image = cv2.GaussianBlur(img, (3, 3), 0)
     return denoised_image
 
-# 创建用于GUI展示的image并调整其大小以适应界面
-def show_image(img):
+def show_image(img, name):
 
     if len(img.shape) == 2:
         h, w = img.shape
@@ -36,79 +31,99 @@ def show_image(img):
         h, w, _ = img.shape
 
     ratio = min(1000 / h, 1000 / w)
-    cv2.imshow("img", cv2.resize(img, (int(w * ratio), int(h * ratio))))
+    cv2.imshow(f"{name}", cv2.resize(img, (int(w * ratio), int(h * ratio))))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    # image_show = cv2.resize(img, (int(w * ratio), int(h * ratio)))
-    # label.config(image=image_show)
-    # label.image = image_show
 
-# 边缘检测
+def remove_shadow(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # 定义阴影的HSV范围
+    lower_shadow = np.array([0, 0, 115], dtype=np.uint8)
+    upper_shadow = np.array([255, 33, 207], dtype=np.uint8)
+    # 创建阴影掩码
+    shadow_mask = cv2.inRange(hsv, lower_shadow, upper_shadow)
+    # 去除阴影
+    img_no_shadow = cv2.bitwise_and(img, img, mask=~shadow_mask)
+    # 将阴影区域改为白色
+    img_no_shadow[~shadow_mask == 0] = [255, 255, 255]
+
+    return img_no_shadow
+
+def extract_color(image, color_name, color_dict):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_bound, upper_bound = color_dict[color_name]
+    mask = cv2.inRange(hsv, np.array(lower_bound), np.array(upper_bound))
+    result = cv2.bitwise_and(image, image, mask=mask)
+    return result
+
 def detect_edges(img):
-   # global edge
-    #gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    edge = cv2.Canny(img, 15, 20)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    show_image(img, 'nnn ')
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    edge = cv2.Canny(img, 100, 200)
+   # # 霍夫变换
+   #  lines = cv2.HoughLinesP(edge, 1, np.pi / 180, threshold=30, minLineLength=10, maxLineGap=25)
+   #
+   #  # 绘制连接的线条到空白图像
+   #  connected_edge = np.zeros_like(img)
+   #  for line in lines:
+   #      x1, y1, x2, y2 = line[0]
+   #      cv2.line(connected_edge, (x1, y1), (x2, y2), (255, 255, 255), 2)
     return edge
 
-def segement(color, edge):
-    rect = color.copy()
-    # edges = cv2.Canny(image, 30, 100)
-
-    # cv2.imwrite('edge.png', edges)
-
+def find_contours(edge):
     # 寻找轮廓
-    contours, _ = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     # 过滤轮廓
     filtered_contours = []
     for contour in contours:
-        area = cv2.contourArea(contour)
-        if area > 75.0:  # 根据实际情况调整面积阈值
+        perimeter = cv2.arcLength(contour, True)
+        if perimeter > 10000:
             filtered_contours.append(contour)
 
-    # 切分图像
+    return contours
+
+def draw_contours(img, contours):
+    cv2.drawContours(img, contours, -1, (0, 0, 0), 5)
+
+# 切分图像
+def segement(original_image, filtered_contours):
+    rect = original_image.copy()
     segmented_images = []
     for i, contour in enumerate(filtered_contours):
         x, y, w, h = cv2.boundingRect(contour)
-        roi = color[y:y + h, x:x + w]
-        segmented_images.append(roi)
-        cv2.rectangle(rect, [x, y], [x + w, y + h], (0, 0, 250), 5)
+        if w*h > 50000:
+            roi = original_image[y:y + h, x:x + w]
+            segmented_images.append(roi)
+            cv2.rectangle(rect, [x, y], [x + w, y + h], (0, 0, 250), 5)
         #cv2.imwrite(f"seg/segmented_image_{i}.png", roi)
 
     cv2.imwrite('rect.png', rect)
+
     return rect
 
-#def trigger():
-select_image('images/test_images/1.jpg')
-#show_image(image)
 
-denoised_image = denoise(image)
-#show_image(denoised_image)
-
-edge = detect_edges(denoised_image)
-show_image(edge)
-cv2.imwrite('canny6.png', edge)
-
-rect = segement(image, edge)
-show_image(rect)
+# 定义颜色字典
+color_thresholds = {
+    'red': ([0, 100, 100], [10, 255, 255]),
+    'yellow': ([20, 100, 100], [30, 255, 255]),
+    'blue': ([100, 100, 100], [120, 255, 255]),
+    'green': ([70, 40, 40], [80, 255, 255]),
+    'purple': ([130, 50, 50], [160, 255, 255])
+}
 
 
+select_image('images/test_images/3.jpg')
 
-# clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-# img_contrast = clahe.apply(img)
+green_result = extract_color(image, 'green', color_thresholds)
+show_image(green_result, 'green')
 
-# # 创建主窗口
-# root = tk.Tk()
-# root.title("Lego元件计数器")
-#
-# # 创建标签以显示图片
-# label = tk.Label(root)
-# label.pack(padx=10, pady=10)
-#
-# # 创建按钮以选择图片
-# select_button = tk.Button(root, text="请选择图片", command=trigger)
-# select_button.pack(padx=10, pady=5)
-#
-# # 运行主循环
-# root.mainloop()
+#green_result = cv2.cvtColor(green_result, cv2.COLOR_HSV2GRAY)
+edge = detect_edges(green_result)
+show_image(edge, 'edge')
 
-#trigger()
+green_result = cv2.cvtColor(green_result, cv2.COLOR_BGR2GRAY)
+contours = find_contours(green_result)
+rect = segement(image, contours)
+show_image(rect, 'rect')
+
